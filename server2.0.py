6244a24a04b1da73f03ea8052d9200f524558716
Jaleel Rogers <86129229+CaptainBlast096@users.noreply.github.com>
2023-11-05 21:55:11 -0500

    Create server2.0.py

---
+++ server2.0.py
@@ -0,0 +1,51 @@
+import socket
+import os
+
+IP = socket.gethostbyname(socket.gethostname())
+PORT = 4455
+ADDR = (IP, PORT)
+FORMAT = "utf-8"
+SIZE = 1024
+DESTINATION_FOLDER = "/home/jaleel/Python/Database"
+
+def recieve_file(conn):
+      # Receive the file name from the client
+    filename = conn.recv(SIZE).decode(FORMAT)
+
+    # Inform the client that the server is ready to receive the file
+    conn.send("READY".encode(FORMAT))
+
+    # Set the file path in the destination folder
+    file_path = os.path.join(DESTINATION_FOLDER, filename)
+
+    with open(file_path, "wb") as file:
+        while True:
+            data = conn.recv(SIZE)
+            if data == b'UPLOAD_COMPLETE':
+                break
+            file.write(data)
+
+    # Inform the client that the file transfer is complete
+    conn.send("File uploaded successfully".encode(FORMAT))
+
+def main():
+    print("[START] Server is starting.")
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(ADDR)
+    server.listen()
+    print("[LISTENING] Server is listening.")
+
+    while True:
+          conn, addr = server.accept()
+          print(f"[NEW CONNECTION] {addr} connected.")
+
+          #filename = conn.recv(SIZE).decode(FORMAT)
+          #print("[RECV] Filename received.")
+          #file = open(filename, "w")
+          #conn.send("Filename received." .encode(FORMAT))
+          # Receive the file from the client and save it to the destination folder
+          recieve_file(conn)
+          conn.close
+        
+if __name__== "__main__":
+        main()
