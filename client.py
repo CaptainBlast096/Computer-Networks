5fff4c248e189c354345e54cbca6fb119ed80cba
Jaleel Rogers <86129229+CaptainBlast096@users.noreply.github.com>
2023-11-06 23:55:12 -0500

    Update client.py
    Add comments

--- client.py
+++ client.py
@@ -1,55 +1,63 @@
-import os
+""""
+Author: Jaleel Rogers
+Class: CNT3004.01
+Proffesor: Dr. Dina
+Title: HW #3 - Server
+Date: 11/10/23
+"""""
 import socket
 
-# IP = "192.168.1.101" #"localhost"
-IP = "localhost"
+IP = socket.gethostbyname(socket.gethostname())
 PORT = 4450
 ADDR = (IP, PORT)
-SIZE = 1024  ## byte .. buffer size
 FORMAT = "utf-8"
-SERVER_DATA_PATH = r"C:\Database"
-
+SIZE = 1024
+SENDER_FOLDER = "/home/jaleel/Python/Desktop" # Not needed in program
 
 def main():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect(ADDR)
-    while True:  ### multiple communications
+    
+    while True:
         data = client.recv(SIZE).decode(FORMAT)
         cmd, msg = data.split("@")
-        if cmd == "OK":
-            print(f"{msg}")
-        elif cmd == "DISCONNECTED":
-            print(f"{msg}")
-            break
 
-        data = input("> ")
-        data = data.split(" ")
+        if cmd == "DISCONNECTED":
+             print(f"[SERVER]: {msg}")
+             break
+        
+        elif cmd == "OK":
+             print(f"{msg}")
+
+        data = input("> ") 
+        data = data.split(" ") # Splitting by space
         cmd = data[0]
 
-        if cmd == "TASK":
-            client.send(cmd.encode(FORMAT))
-
-        elif cmd == "UPLOAD":
-            filename = data[1]
-            try:
-                with open(SERVER_DATA_PATH, 'r') as file:
-                    file_data = file.read()
-                    client.send(filename.encode(FORMAT))
-                    msg = client.recv(SIZE).decode(FORMAT)
-                    print(f"[SERVER]: {msg}")
-                    client.send(file_data.encode(FORMAT))
-                    msg = client.recv(SIZE).decode(FORMAT)
-                    print(f"[SERVER]: {msg}")
-            except FileNotFoundError:
-                print(f"File '{filename}' not found.")
-
+        if cmd == "HELP":
+            client.send(cmd.encode((FORMAT)))
+        
         elif cmd == "LOGOUT":
             client.send(cmd.encode(FORMAT))
             break
+        
+        elif cmd == "LIST": 
+            client.send(cmd.encode(FORMAT))
+        
+        elif cmd == "UPLOAD":
+            path = data[1]
 
+            with open(f"{path}", "r") as f:
+                text_file = f.read()
+            
+            file_name = path.split("/")[-1]
+            send_data = f"{cmd}@{file_name}@{text_file}"
+            client.send(send_data.encode(FORMAT))
+
+        elif cmd == "DELETE":
+            client.send(f"{cmd}@{data[1]}".encode(FORMAT))
+        
     print("Disconnected from the server.")
-    client.close()  ## close the connection
-
-
-if __name__ == "__main__":
-    main()
+    client.close()
+        
+if __name__== "__main__":
+        main()
